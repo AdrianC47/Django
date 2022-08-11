7eab063b1f27ed4e9da90b9ad76ce6469ef97a84
Adrian <luiscabreraucme@hotmail.com>
2022-08-11 15:01:47 -0500

    Ejecutar proyecto desde entorno local

--- dj/empleado/empleado/settings/local.py
+++ dj/empleado/empleado/settings/local.py
@@ -1,10 +1,12 @@
 # Aqui va la configuracion de mi entorno local
 from .base import*
 # SECURITY WARNING: don't run with debug turned on in production!
+#Con esto  indico que estoy en un entorno de desarrollo y no en un entorno de produccion
 DEBUG = True
 
 ALLOWED_HOSTS = []
 
+#Pongo la base ya que se trabajará con una propia y no con una de producción
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
