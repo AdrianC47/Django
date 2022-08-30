bfa9f45839374c66a8c12dfa83c4ab72991f3cd2
Adrian <luiscabreraucme@hotmail.com>
2022-08-30 00:27:59 -0500

    Personalizar Tablas en el Administrador Django

--- dj/empleado/applications/persona/admin.py
+++ dj/empleado/applications/persona/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
-from .models import Empleado, Habilidad
+from .models import Empleado, Habilidades
 # Register your models here.
-admin.site.register(Empleado)
-admin.site.register(Habilidad)
+
+admin.site.register(Habilidades)
+
+class EmpleadoAdmin(admin.ModelAdmin): #Le digo que quiero que trabaje con herramientas que 
+        list_display =(                #nos ofrece el administrador de Django mediante el admin.ModelAdmin
+            'first_name',
+            'last_name',
+            'departamento',
+            'job',
+        )    
+
+admin.site.register(Empleado, EmpleadoAdmin)
--- dj/empleado/applications/persona/models.py
+++ dj/empleado/applications/persona/models.py
@@ -3,6 +3,16 @@ from django.db import models
 #
 from applications.departamento.models import Departamento
 # Create your models here.
+
+class Habilidades(models.Model):
+    habilidad = models.CharField("Habilidad", max_length=50)
+
+    class Meta:
+        verbose_name = 'Habilidad'
+        verbose_name_plural ='Habilidades Empleados'
+    def __str__(self):
+        return str(self.id) + '-' + self.habilidad
+        
 class Empleado(models.Model):
     """ Modelo para tabla empleado"""
 
@@ -17,25 +27,18 @@ class Empleado(models.Model):
     # Economista
     # Otro 
     first_name = models.CharField("Nombres", max_length=60)
-    last_name = models.CharField("Apelidos", max_length=60)
+    last_name = models.CharField("Apellidos", max_length=60)
     job = models.CharField("Trabajo", max_length=1, choices=JOB_CHOICES)
     departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE)
-    #image = models.ImageField(, upload_to=None, height_field=None, width_field=None, max_length=None)
+    avatar = models.ImageField(upload_to='empleado', blank= True,null= True)
+    habilidades = models.ManyToManyField(Habilidades)#Relacion muchos a muchos
 
     class Meta:
         verbose_name = 'Mi Empleado'
         verbose_name_plural ='Empleados de la Empresa'
-        ordering = ['last_name']
-        unique_together = ('last_name','first_name')    
+        ordering = ['-first_name','last_name']
+        unique_together = ('first_name','departamento')    
     
     def __str__(self):
         return str(self.id) + '-' + self.first_name + '-' +self.last_name 
 
-class Habilidad(models.Model):
-    texto = models.CharField("Texto", max_length=60)
-
-    class Meta:
-        verbose_name = 'Mi Departamento'
-        verbose_name_plural ='Habilidades'
-    def __str__(self):
-        return str(self.id) + '-' + self.texto
