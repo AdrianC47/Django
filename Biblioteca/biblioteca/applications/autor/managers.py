0b7d721db92762c8f12d46ca51d6d6dc5ae76ac7
Adrian <luiscabreraucme@hotmail.com>
2022-09-06 11:34:29 -0500

    Operador OR en una consulta de la BD

--- Biblioteca/biblioteca/applications/autor/managers.py
+++ Biblioteca/biblioteca/applications/autor/managers.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from django.db.models import Q ##Esta funcion me va a ayudar a hacer las sentencias  de tipo OR
+
 #Crearemos una clase la cual que represente todas las funciones que van a pertenecer a ese modelo autor
 
 class AutorManager (models.Manager):
@@ -9,6 +11,14 @@ class AutorManager (models.Manager):
 
         resultado=self.filter(
             nombre__icontains= kword # esto busca cada caracter en la BD si es similar, es decir si contiene una a , t ,etc
+        )
+
+        return resultado
+
+    def buscar_autor2(self, kword):
+
+        resultado=self.filter(
+            Q(nombre__icontains= kword) | Q(apellidos__icontains = kword)
         )
 
         return resultado
