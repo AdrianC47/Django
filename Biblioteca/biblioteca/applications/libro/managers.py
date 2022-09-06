10c667ffe10e3b66ccc1c6a84733834a2133cc37
Adrian <luiscabreraucme@hotmail.com>
2022-09-06 12:46:31 -0500

    Filtro por Rango de Fechas con Validacion de formato de fecha

--- Biblioteca/biblioteca/applications/libro/managers.py
+++ Biblioteca/biblioteca/applications/libro/managers.py
@@ -1,3 +1,4 @@
+import datetime
 from statistics import mode
 from django.db import models
 
@@ -9,6 +10,20 @@ class LibroManager(models.Manager):
         resultado=self.filter(
             titulo__icontains= kword,
             fecha__range = ('2000-04-21','2020-05-25')
+        )
+
+        return resultado
+
+    def listar_libros2(self, kword,fecha1,fecha2):
+    
+    #Siempre es recomendable validar lo que sería fechas debido al formato de algunos navegadores
+
+        date1 = datetime.datetime.strptime(fecha1,"%Y-%m-%d").date
+        date2 = datetime.datetime.strptime(fecha2,"%Y-%m-%d").date
+
+        resultado=self.filter(
+            titulo__icontains= kword,
+            fecha__range = (date1,date2)
         )
 
         return resultado
--- Biblioteca/biblioteca/applications/libro/views.py
+++ Biblioteca/biblioteca/applications/libro/views.py
@@ -13,7 +13,15 @@ class ListLibros(ListView):
 
     def get_queryset(self):
         palabra_clave = self.request.GET.get("kword",'')
-        
-        return Libro.objects.listar_libros(palabra_clave)
+        #fecha 1
+        f1 = self.request.GET.get("fecha1",'')
+        #fecha 2
+        f2 = self.request.GET.get("fecha2",'')
+
+        if f1 and f2:
+            return Libro.objects.listar_libros2(palabra_clave,f1,f2)
+        else:
+            return Libro.objects.listar_libros(palabra_clave)
+
 
 
