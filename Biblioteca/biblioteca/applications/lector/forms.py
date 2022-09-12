123e381809d0da8b96a4ca9992d87b95a82352fa
Adrian <luiscabreraucme@hotmail.com>
2022-09-12 08:33:54 -0500

    Método Create y Save para  guardar datos

--- Biblioteca/biblioteca/applications/lector/forms.py
+++ Biblioteca/biblioteca/applications/lector/forms.py
@@ -3,10 +3,10 @@ from django import forms
 from .models import Prestamo
 
 class PrestamoForm(forms.ModelForm):
-
-    class Meta: #Usamos la clase Meta para poder trabajar directamente con el modelo 
+ #Usamos la clase Meta para poder trabajar directamente con el modelo 
+    class Meta: #En este caso los metadatos lo que van a a hacer es conectar directametne a nuestro modelo prestamo y convertir cada campo en un formulario HTML (internamente)
         model  = Prestamo
-        fields =(
+        fields =( #son los campos
             'lector',
             'libro'
         )
--- Biblioteca/biblioteca/applications/lector/views.py
+++ Biblioteca/biblioteca/applications/lector/views.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from django.shortcuts import render
 from django.views.generic.edit import FormView
 
@@ -10,7 +12,25 @@ class RegistrarPrestamo(FormView):
     form_class = PrestamoForm
     success_url = "." #redirecciono a la misma pagina
 
-    def form_valid(self, form):
+    def form_valid(self, form): 
+    # De esta forma puedo hacer el registro del objeto 
+    # Esto crea desde cero un objeto
+    #     Prestamo.objects.create(
+    #         lector = form.cleaned_data['lector'],
+    #         libro = form.cleaned_data['libro'],
+    #         fecha_prestamo = date.today(),
+    #         devuelto = False
+    #     )
 
+    # Esta es otra forma tambien
+    # Pero el Save cuando no existe obvio si srea un nuevo registro sin embargo si ésta variable préstamo la cual estamos haciendo el llamado
+    # ya existia en la BD, es decir lo hemos recuperado pues este save estuviera actualizando el registro y no creando una nueva instancia
+        prestamo = Prestamo (
+            lector = form.cleaned_data['lector'],
+            libro = form.cleaned_data['libro'],
+            fecha_prestamo = date.today(),
+            devuelto = False
+        )
+        prestamo.save()#Con esto guardo la instancia
 
         return super(RegistrarPrestamo, self).form_valid(form)
