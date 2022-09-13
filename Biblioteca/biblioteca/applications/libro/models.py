2c7f0696648ca8316bd087d70db6f503e22e288a
Adrian <luiscabreraucme@hotmail.com>
2022-09-13 08:43:21 -0500

    Tarea Pre_Save

--- Biblioteca/biblioteca/applications/libro/models.py
+++ Biblioteca/biblioteca/applications/libro/models.py
@@ -1,6 +1,6 @@
 from pickletools import optimize
 from django.db import models
-from django.db.models.signals import post_save #luego de guardar
+from django.db.models.signals import post_save, pre_save # post_save luego de guardar
 
 # apps terceros
 from PIL import Image
@@ -48,12 +48,23 @@ class Libro (models.Model):
 def optimize_image(sender, instance, **kwargs):   #Se pasan los siguientes parametros
 # sender hace referencia hacia donde se ejecuta la funcion
 # instance hace referencia a la instancia o registro sobre el cual estamos trabajando
-# los keywords que generalmente les pasamos en las funciones que utilizamos o trabajos dentro de la ORMde django
+# los keywords que generalmente les pasamos en las funciones que utilizamos o trabajos dentro de la ORM de django
     print('===========================')
     print(instance)
-    if instance.portada:
-        portada = Image.open(instance.portada.path)
-        portada.save(instance.portada.path, quality=20, optimize= True)
+    if instance.portada: #pregunto si a mi instancia(modelo Libro) le estan mandando una portada 
+        portada = Image.open(instance.portada.path) #abro la imagen
+        portada.save(instance.portada.path, quality=20, optimize= True) #optimizacion
 
 
-post_save.connect(optimize_image,sender = Libro)
+# Se conecta la funcion con la que quiero trabajar y el modelo, es decir quiero que se ejecute esta funcion 
+post_save.connect(optimize_image,sender = Libro)   #Cuando se guarde qué registro(Libro) quiero que se ejecute una función (optimize_image)
+
+
+def funcion_para_PreSave(sender, instance, **kwargs):
+    print('***********************************')
+    print(instance)
+    if instance.stock:
+        instance.stock = instance.stock +1
+
+
+pre_save.connect(funcion_para_PreSave, sender = Libro)
