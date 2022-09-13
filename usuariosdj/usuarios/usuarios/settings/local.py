a67a9f6478dc522955d4252c7d2c6193333da651
Adrian <luiscabreraucme@hotmail.com>
2022-09-13 11:02:30 -0500

    Archivo Secret, para esconder información sensible en el proyecto

--- usuariosdj/usuarios/usuarios/settings/local.py
+++ usuariosdj/usuarios/usuarios/settings/local.py
@@ -12,9 +12,9 @@ ALLOWED_HOSTS = []
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'dbuser',
-        'USER': 'adrian',
-        'PASSWORD': 'usuarios2022',
+        'NAME': get_secret("DB_NAME"),
+        'USER': get_secret("USER"),
+        'PASSWORD': get_secret("PASSWORD"),
         'HOST': 'localhost',
         'PORT': '5432',
     }
