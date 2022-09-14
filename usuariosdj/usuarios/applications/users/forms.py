455c0c76c931108bff286a4e785e8827dd538c3c
Adrian <luiscabreraucme@hotmail.com>
2022-09-14 10:44:00 -0500

    Crear Usuario - FormView

--- usuariosdj/usuarios/applications/users/forms.py
+++ usuariosdj/usuarios/applications/users/forms.py
@@ -1,11 +1,37 @@
+from dataclasses import fields
 from django import forms
 from .models import User
 
 
 class UserRegisterForm(forms.ModelForm):
 
+    password1 = forms.CharField(
+        label='Contraseña',
+        required=True,
+        widget= forms.PasswordInput(
+            attrs={
+                'placeholder': 'Contraseña'
+            }
+        )
+    )
+    password2 = forms.CharField(
+        label='Repite la Contraseña',
+        required=True,
+        widget= forms.PasswordInput(
+            attrs={
+                'placeholder': 'Repetir Contraseña'
+            }
+        )
+    )    
     class Meta:
         """Meta definition for UserRegisterform."""
 
         model = User
-        fields = ('__all__')# si quiero que muestre todos los atributos
+        #fields = ('__all__')# si quiero que muestre todos los atributos
+        fields = (
+            'username',
+            'email',
+            'nombres',
+            'apellidos',
+            'genero'
+        )
--- usuariosdj/usuarios/applications/users/views.py
+++ usuariosdj/usuarios/applications/users/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 
-from django.views.generic import CreateView
+from django.views.generic import CreateView, FormView
 
 from .forms import UserRegisterForm
 # Create your views here.
 
 
-class UserRegisterCreateView(CreateView):
+class UserRegisterCreateView(FormView):
     form_class= UserRegisterForm
     template_name = "users/register.html"
     success_url = '/'
