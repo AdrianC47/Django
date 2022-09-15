e97258cf9ba1457c341311f74ae7bdfd2412c4f4
Adrian <luiscabreraucme@hotmail.com>
2022-09-15 11:12:09 -0500

    Mixin Ejemplo Práctico

--- usuariosdj/usuarios/applications/home/views.py
+++ usuariosdj/usuarios/applications/home/views.py
@@ -1,8 +1,21 @@
+import datetime
+#
 from django.shortcuts import render
 
 from django.views.generic import TemplateView
 
+class FechaMixin(object): #Usualmente los mixin deben ir al inicio
+    
+    def get_context_data(self, **kwargs):#Esta funcion es cuando quiero mandar un contexto al template
+        context = super(FechaMixin, self).get_context_data(**kwargs)
+        context['fecha'] = datetime.datetime.now() #contexto fecha
+        return context
 
 
-class HomePage(TemplateView):
+class HomePage(FechaMixin,TemplateView):
     template_name = "home/index.html"
+
+
+
+class TemplatePruebaMixin(FechaMixin,TemplateView):
+    template_name = "home/mixin.html"
