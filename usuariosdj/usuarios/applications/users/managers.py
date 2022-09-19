ccca991ed914424bc76ddc1338a7accf09025c06
Adrian <luiscabreraucme@hotmail.com>
2022-09-19 09:03:28 -0500

    Solucion Is_Active en el Modelo User

--- usuariosdj/usuarios/applications/users/managers.py
+++ usuariosdj/usuarios/applications/users/managers.py
@@ -9,12 +9,13 @@ class UserManager(BaseUserManager,models.Manager):
 # el is_staff hace referencia a que si el usuario que esta creandose puede on no acceder al admin
 # el is_superuser hace referencia a que si el usuario que se esta creando es o no un superusuario
 
-    def _create_user(self, username,email, password, is_staff, is_superuser,**extra_fields): 
+    def _create_user(self, username,email, password, is_staff, is_superuser, is_active,**extra_fields): 
         user = self.model (
             username = username,
             email = email,
             is_staff=is_staff, #son booleanos
             is_superuser=is_superuser, #son booleanos
+            is_active= is_active,
             #el password debo encriptarlo
             **extra_fields
         )
@@ -24,11 +25,11 @@ class UserManager(BaseUserManager,models.Manager):
         return user
 
     def create_user(self, username, email, password=None, **extra_fields):
-        return self._create_user(username, email, password, False, False, **extra_fields)
+        return self._create_user(username, email, password, False, False, False,**extra_fields)
 
     def create_superuser(self,username,email, password,**extra_fields):# los extra_fields hacen referenccia a cualquier otro atributo que se agregue
          #funcion privada
-        return self._create_user(username,email,password,True,True,**extra_fields) #se debe indicar si el usuario es activo(que puede acceder al admin)
+        return self._create_user(username,email,password,True,True, True,**extra_fields) #se debe indicar si el usuario es activo(que puede acceder al admin)
 
     def cod_validation(self, id_user, cod_registro):
         if self.filter(id= id_user,codRegistro = cod_registro).exists():
