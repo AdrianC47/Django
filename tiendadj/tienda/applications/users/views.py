bbf7e3b8c86c8e00436238cd9d1a47c94c861c2f
Adrian <luiscabreraucme@hotmail.com>
2022-09-27 17:00:50 -0500

    ApiView Google Login View

--- tiendadj/tienda/applications/users/views.py
+++ tiendadj/tienda/applications/users/views.py
@@ -1,7 +1,17 @@
+from rest_framework.views import APIView
+#
 from django.shortcuts import render
+#
+#django import
 from django.views.generic import TemplateView
 
+# Serializadores
+from  .serializers import LoginSocialSerializer
 # Create your views here.
 
 class LoginUser(TemplateView):
     template_name = "users/login.html"
+
+
+class GoogleLoginView(APIView):
+    serializer_class = LoginSocialSerializer
